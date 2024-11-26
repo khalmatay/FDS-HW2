25470c40418009874663d1420f1329c0f3683ade
ValentinaEmili <valentinaemili.2002@gmail.com>
2024-11-26 16:01:55 +0100

    Add files via upload

--- Homework02/libs/models/poor_cnn.py
+++ Homework02/libs/models/poor_cnn.py
@@ -5,8 +5,8 @@ class PoorPerformingCNN(nn.Module):
     def __init__(self):
         super(PoorPerformingCNN, self).__init__()
         ##############################
-        ###     CHANGE THIS CODE   ###
-        ##############################  
+        
+        
         self.conv1 = nn.Conv2d(3, 4, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
@@ -22,3 +22,4 @@ class PoorPerformingCNN(nn.Module):
         x = x.view(-1, 8 * 8 * 8)
         x = self.fc1(x)
         return x
+        ##############################  
